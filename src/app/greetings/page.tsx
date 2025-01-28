1d1f2aa2348a23199792855364c104366b94dc44
RobsonFe <robson_20ferreira@hotmail.com>
2025-01-28 00:40:04 -0300

    atualiza mensagem de boas-vindas na página de saudações para descrever melhor o sistema de aluguel de veículos

--- src/app/greetings/page.tsx
+++ src/app/greetings/page.tsx
@@ -4,7 +4,7 @@ const greetings = () => {
 	return (
 		<Layout title="Easy Drive">
 			<div className="grid grid-cols-1">
-				<p className="text-lg p-5">Bem Vindos ao Sistema de Alugéis de Carro.</p>
+				<p className="text-lg p-5">Bem Vindos ao Sistema de Alugueis de Veículos Easy Drive. Nosso sistema oferece uma plataforma intuitiva e fácil de usar para alugar veículos de forma rápida e segura. Com uma ampla variedade de carros disponíveis, garantimos que você encontrará o veículo perfeito para suas necessidades. Aproveite nossas ofertas exclusivas e tenha uma experiência de aluguel sem complicações. Easy Drive, sua melhor escolha para mobilidade.</p>
 			</div>
 		</Layout>
 	)
