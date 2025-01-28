f781f7efbd52b8dc6170d32b27f4b706dba1751e
RobsonFe <robson_20ferreira@hotmail.com>
2025-01-28 00:33:19 -0300

    refatora o serviço para melhorar a legibilidade e adiciona método para criação de veículos

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,42 +1,54 @@
-import axios from "axios";
+import axios from "axios"
 
 class Service {
-    private api: string;
+  private api: string
 
-     constructor(api: string = 'http://127.0.0.1:8000/api/v1/') {
-        this.api = api;
-    }
-    
-    async rentList() {
-        try {
-            const response = await axios.get(`${this.api}rent/list/`);
-					const result = response.data.results;
-            return result;
-            
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                console.error('Erro na requisição:', error.message);
-            } else {
-                console.error('Erro inesperado:', error);
-            }
-            throw error;
-        }
-    }
+  constructor(api: string = "http://127.0.0.1:8000/api/v1/") {
+    this.api = api
+  }
 
-    async userInfo() {
-        try {
-            const response = await axios.get(`${this.api}client/user/list/`);
-            console.log(response.data.results);
-            return response.data.results;
-
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                console.error('Erro na requisição:', error.message);
-            } else {
-                console.error('Erro inesperado:', error);
-            }
-            throw error;
-        }
+  async rentList() {
+    try {
+      const response = await axios.get(`${this.api}rent/list/`)
+      const result = response.data.results
+      return result
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error("Erro na requisição:", error.message)
+      } else {
+        console.error("Erro inesperado:", error)
+      }
+      throw error
     }
+  }
+
+  async vehicleCreate(data: any) {
+    try {
+      const response = await axios.post(`${this.api}vehicle/create/`, data)
+      return response.data
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error("Erro na requisição:", error.message)
+      } else {
+        console.error("Erro inesperado:", error)
+      }
+      throw error
+    }
+  }
+
+  async userInfo() {
+    try {
+      const response = await axios.get(`${this.api}client/user/list/`)
+      console.log(response.data.results)
+      return response.data.results
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error("Erro na requisição:", error.message)
+      } else {
+        console.error("Erro inesperado:", error)
+      }
+      throw error
+    }
+  }
 }
-export default Service;
+export default Service
