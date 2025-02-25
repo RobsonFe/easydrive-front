d655285e899b8ff64c694912e40e711751dfa359
RobsonFe <robson_20ferreira@hotmail.com>
2025-02-24 22:26:10 -0300

    adiciona configuração do axios para chamadas à API e implementa interceptor de requisições para inclusão de token de autenticação

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,24 @@
-export const api = "http://127.0.0.1:8000/api/v1/";
+import axios from "axios"
+export const baseApiUrl = "http://127.0.0.1:8000/api/v1/"
+
+const api = axios.create({
+  baseURL: baseApiUrl,
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token")
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+export default api
